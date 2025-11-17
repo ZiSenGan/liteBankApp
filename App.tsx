45df21554ca015c59f28f54b410182edcd553c7c
ZiSenGan <40378190+ZiSenGan@users.noreply.github.com>
2025-11-17 15:47:42 +0800

    add react lazy

--- App.tsx
+++ App.tsx
@@ -1,27 +1,37 @@
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
-import HomeScreen from './src/libs/home';
 import Login from './src/libs/login';
-import TransferScreen from './src/libs/transfer';
 import { RootStackParamList } from './src/types';
 import { Loading } from './src/components/LoadingScreen';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import React, { Suspense } from 'react';
 
 const Stack = createStackNavigator<RootStackParamList>();
 
 const queryClient = new QueryClient();
 
+const HomeScreen = React.lazy(() => import('./src/libs/home'));
+const TransferScreen = React.lazy(() => import('./src/libs/transfer'));
 
 export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName='Login'>
-          <Stack.Screen name="Login" component={Login} />
-          <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false, gestureEnabled: false }} />
-          <Stack.Screen name="Transfer" component={TransferScreen} options={{ headerShown: false }} />
-        </Stack.Navigator>
-
+        <Suspense fallback={<Loading />}>
+          <Stack.Navigator initialRouteName="Login">
+            <Stack.Screen name="Login" component={Login} />
+            <Stack.Screen
+              name="Home"
+              component={HomeScreen}
+              options={{ headerShown: false, gestureEnabled: false }}
+            />
+            <Stack.Screen
+              name="Transfer"
+              component={TransferScreen}
+              options={{ headerShown: false }}
+            />
+          </Stack.Navigator>
+        </Suspense>
         <Loading />
       </NavigationContainer>
     </QueryClientProvider>
